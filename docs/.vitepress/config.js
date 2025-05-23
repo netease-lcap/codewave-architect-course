e9bb8b9459fc5e89f2e4c126ebba93789f4a993e
xiaran01 <xiaran01@corp.netease.com>
2025-05-23 10:37:07 +0800

    docs :  update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -23,36 +23,36 @@ module.exports = {
                     text: '部署集成',
                     link: '/deployment/',
                     items: [
-                        { text: '源码导出', link: '/deployment/index' },
+                        { text: '源码导出 ⏳', link: '/deployment/index' },
                         { text: '后端翻译器', link: '/deployment/backend-generator-plugin' },
-                        { text: '前端翻译器', link: '/deployment/index' },
-                        { text: '日志监控', link: '/deployment/index' },
+                        { text: '前端翻译器 ⏳', link: '/deployment/index' },
+                        { text: '日志监控 ⏳', link: '/deployment/index' },
                     ]
                 },
                 {
                     text: '应用架构',
                     link: '/architecture',
                     items: [
-                        { text: '微前端', link: '/architecture' },
-                        { text: '微服务', link: '/architecture' },
-                        { text: '认证授权', link: '/architecture' },
-                        { text: '分布式事务', link: '/architecture' }
+                        { text: '微前端 ⏳', link: '/architecture' },
+                        { text: '微服务 ⏳', link: '/architecture' },
+                        { text: '认证授权 ⏳', link: '/architecture' },
+                        { text: '分布式事务 ⏳', link: '/architecture' }
                     ]
                 },
                 {
                     text: '平台定制',
                     link: '/platform',
                     items: [
-                        { text: '管控面定制', link: '/platform' },
-                        { text: 'IDE定制', link: '/platform' }
+                        { text: '管控面定制 ⏳', link: '/platform' },
+                        { text: 'IDE定制 ⏳', link: '/platform' }
                     ]
                 },
                 {
-                    text: '安全强化',
+                    text: '安全强化 ⏳',
                     link: '/security',
                 },
                 {
-                    text: '性能优化',
+                    text: '性能优化 ⏳',
                     link: '/performance',
                 },
             ]
--- docs/development/sidebar.js
+++ docs/development/sidebar.js
@@ -7,8 +7,8 @@ module.exports =
         {
             text: '连接器开发', link: '/development/connector',
         },
-        { text: '组件样式定制', link: '/getting - started' },
-        { text: '流程扩展定制', link: '/getting - started' },
+        { text: '组件样式定制 ⏳', link: '/getting - started' },
+        { text: '流程扩展定制 ⏳', link: '/getting - started' },
         { text: '数据库插件', link: '/development/owl' }
     ]
 
