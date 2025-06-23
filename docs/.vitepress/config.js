9af8d630c90515f31ab0b03c8d4277e2cdabc3fb
xiaran01 <xiaran01@corp.netease.com>
2025-06-23 10:10:46 +0800

    add practise

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -12,66 +12,71 @@ module.exports = {
             { text: '应用开发', link: '/development' },
             { text: '部署集成', link: '/deployment' },
             { text: '应用架构', link: '/architecture' },
-            { text: '平台定制', link: '/platform' }
+            { text: '平台定制', link: '/platform' },
+            { text: '最佳实践', link: '/practice/index' }
         ],
-        sidebar:
-            [
-                {
-                    text: '应用开发',
-                    link: '/development/',
-                    items: [
-                        {
-                            text: '服务端扩展', link: '/development/serverend',
-                        },
-                        { text: '前端扩展', link: '/development/frontend' },
-                        {
-                            text: '连接器开发', link: '/development/connector',
-                        },
-                        { text: '组件样式定制', link: '/development/componentsStyle' },
-                        // { text: '流程扩展定制 ⏳', link: '/development/pocess' },
-                        { text: '数据库插件', link: '/development/owl' }
-                    ]
-                },
-                {
-                    text: '部署集成',
-                    link: '/deployment/',
-                    items: [
-                        { text: '源码导出 ', link: '/deployment/export' },
-                        { text: '多应用集成独立部署', link: '/deployment/multi-application-deploy' },
-                        { text: '后端翻译器 ', link: '/deployment/backend-generator-plugin' },
-                        { text: '前端翻译器 ', link: '/deployment/frontend-generator-plugin' },
-                        { text: '日志监控', link: '/deployment/log-monitor' },
-                    ]
-                },
-                {
-                    text: '应用架构',
-                    link: '/architecture',
-                    items: [
-                        { text: '微前端 ⏳', link: '/architecture' },
-                        { text: '认证授权', link: '/architecture/authentication' },
-                        { text: '分布式事务 ', link: '/architecture/multi-application-transaction' },
-                        { text: '微服务架构对接 ', link: '/architecture/microservice' },
-                        { text: '案例：多应用', link: '/architecture/multi-application-case' },
-                        { text: '案例：轻量级微服务', link: '/architecture/micro-application-case' },
-                        { text: '案例：典型微服务', link: '/architecture/micro-application-case' },
-                    ]
-                },
-                {
-                    text: '平台定制',
-                    link: '/platform',
-                    items: [
-                        { text: '管控面定制 ⏳', link: '/platform' },
-                        { text: 'IDE定制 ⏳', link: '/platform' }
-                    ]
-                },
-                {
-                    text: '安全强化 ⏳',
-                    link: '/security',
-                },
-                {
-                    text: '性能优化 ⏳',
-                    link: '/performance',
-                },
-            ]
+        sidebar: {
+            '/practice/': require('../practice/sidebar.js'),
+            '/':
+                [
+                    {
+                        text: '应用开发',
+                        link: '/development/',
+                        items: [
+                            {
+                                text: '服务端扩展', link: '/development/serverend',
+                            },
+                            { text: '前端扩展', link: '/development/frontend' },
+                            {
+                                text: '连接器开发', link: '/development/connector',
+                            },
+                            { text: '组件样式定制', link: '/development/componentsStyle' },
+                            // { text: '流程扩展定制 ⏳', link: '/development/pocess' },
+                            { text: '数据库插件', link: '/development/owl' }
+                        ]
+                    },
+                    {
+                        text: '部署集成',
+                        link: '/deployment/',
+                        items: [
+                            { text: '源码导出 ', link: '/deployment/export' },
+                            { text: '多应用集成独立部署', link: '/deployment/multi-application-deploy' },
+                            { text: '后端翻译器 ', link: '/deployment/backend-generator-plugin' },
+                            { text: '前端翻译器 ', link: '/deployment/frontend-generator-plugin' },
+                            { text: '日志监控', link: '/deployment/log-monitor' },
+                        ]
+                    },
+                    {
+                        text: '应用架构',
+                        link: '/architecture',
+                        items: [
+                            { text: '微前端 ⏳', link: '/architecture' },
+                            { text: '认证授权', link: '/architecture/authentication' },
+                            { text: '分布式事务 ', link: '/architecture/multi-application-transaction' },
+                            { text: '微服务架构对接 ', link: '/architecture/microservice' },
+                            { text: '案例：多应用', link: '/architecture/multi-application-case' },
+                            { text: '案例：轻量级微服务', link: '/architecture/micro-application-case' },
+                            { text: '案例：典型微服务', link: '/architecture/micro-application-case' },
+                        ]
+                    },
+                    {
+                        text: '平台定制',
+                        link: '/platform',
+                        items: [
+                            { text: '管控面定制 ⏳', link: '/platform' },
+                            { text: 'IDE定制 ⏳', link: '/platform' }
+                        ]
+                    },
+                    {
+                        text: '安全强化 ⏳',
+                        link: '/security',
+                    },
+                    {
+                        text: '性能优化 ⏳',
+                        link: '/performance',
+                    },
+                ]
+        },
+
     }
 }
---
+++ docs/practice/sidebar.js
@@ -0,0 +1,82 @@
+module.exports = [
+    {
+        text: '项目管理', link: '/practice/index', items: [
+            {
+                text: '敏捷开发', link: '/practice/index', collapsed: true, items: [
+                    {
+                        text: '用户故事梳理', link: '/practice/index',
+                    },
+                    { text: '进度对齐', link: '/practice/index' },
+                    {
+                        text: '需求优先级', link: '/practice/index',
+                    },
+                    {
+                        text: '需求优先级', link: '/practice/index',
+                    },
+                    {
+                        text: '迭代成果验证', link: '/practice/index',
+                    },
+                    {
+                        text: '版本规划', link: '/practice/index',
+                    },
+                    {
+                        text: '增量交付评估', link: '/practice/index',
+                    },
+                ]
+            },
+            {
+                text: '快速原型法', link: '/practice/index', collapsed: true, items: [
+                    {
+                        text: '快速原型设计', link: '/practice/index',
+                    },
+                    { text: '跨团队协作', link: '/practice/index' },
+                    {
+                        text: '测试收集反馈', link: '/practice/index',
+                    },
+                    {
+                        text: '原型迭代', link: '/practice/index',
+                    },
+                    {
+                        text: '需求收敛与筛选', link: '/practice/index',
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        text: '架构设计', link: '/practice/index', items: [
+            {
+                text: '微服务', link: '/practice/index',
+            },
+            { text: '微前端', link: '/practice/index' },
+            {
+                text: 'DevOps', link: '/practice/index',
+            },
+        ]
+    },
+    {
+        text: '应用开发实践', link: '/practice/index', items: [
+            {
+                text: '低代码开发', link: '/practice/index', collapsed: true, items: [
+                    {
+                        text: '高保真UI还原', link: '/practice/index',
+                    },
+                    { text: '领域建模', link: '/practice/index' },
+                    {
+                        text: '工作流设计', link: '/practice/index',
+                    },
+                ]
+            },
+            {
+                text: '高代码开发', link: '/practice/index', collapsed: true, items: [
+                    {
+                        text: '前端组件库', link: '/practice/index',
+                    },
+                    { text: 'Java依赖库', link: '/practice/index' },
+                    {
+                        text: '连接器', link: '/practice/index',
+                    }]
+            },
+        ]
+    },
+]
